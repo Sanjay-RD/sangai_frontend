b25d068d694cade04d27a4dc26b9289fab1ada50
Sanjay-RD <rishidevsanjay9@gmail.com>
2022-08-25 00:25:00 +0545

    tailwind css setupd

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <div className="underline text-4xl">Welcome From Tailwind Css Test </div>
+  );
 }
 
 export default HomePage;
