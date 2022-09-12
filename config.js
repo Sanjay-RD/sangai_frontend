cab81fab72ea507623db3a92c2679eb1c1070fda
Sanjay-RD <rishidevsanjay9@gmail.com>
2022-09-12 15:07:13 +0545

    live url added

--- config.js
+++ config.js
@@ -2,7 +2,11 @@ import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
 
 export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://sangaiapi.biidhan.com/api"
-  : "http://localhost:5000/api";
+  ? publicRuntimeConfig.API_PRODUCTION
+  : publicRuntimeConfig.API_DEVELOPMENT;
+
+export const baseUrl = publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.BASE_URL_PRODUCTION
+  : publicRuntimeConfig.BASE_URL_DEVELOPMENT;
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
